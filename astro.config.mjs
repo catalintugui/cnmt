92464a1979cea5a62e433c5cc385ed220424eaa3
Catalin Tugui <catalindtugui@gmail.com>
2025-08-01 12:17:28 +0300

    gh pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,6 @@
 import { defineConfig } from "astro/config";
-import github from "@astrojs/github";
 
 export default defineConfig({
-    output: "static",
-    adapter: github(),
     base: "/cnmt/",
+    output: "static",
 });
